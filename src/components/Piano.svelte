a0530f001fc8677f9a1074a31ca0928a6b5e8ed5
Henrik <henrik.zenkert@gmail.com>
2024-10-22 23:04:06 +0200

    dynamic piano width

--- src/components/Piano.svelte
+++ src/components/Piano.svelte
@@ -1,267 +1,293 @@
 <script>
-	import touchable from "./touchable.js";
-	import createSampler from "./sampler.js";
-	import Overlay from "./Overlay.svelte";
-	import Window from "./Window.svelte";
-	import { translation } from "./keybindings";
-	export let keys;
-	export let fullscreen;
-	export let showKeybindings;
-	export let showNotes;
-	// If sound resources have not loaded yet we use this in triggering a message that is has not loaded.
-	$: showSoundMessage = false;
+  import touchable from "./touchable.js";
+  import createSampler from "./sampler.js";
+  import Overlay from "./Overlay.svelte";
+  import Window from "./Window.svelte";
+  import { translation } from "./keybindings";
+  export let keys;
+  export let fullscreen;
+  export let showKeybindings;
+  export let showNotes;
+  // If sound resources have not loaded yet we use this in triggering a message that is has not loaded.
+  $: showSoundMessage = false;
 
-	$: width = keys.length * 100 + 2;
+  $: width = keys.length * 100 + 2;
 
-	let clicked = null;
-	let touches = [];
-	let pressed = [];
+  let clicked = null;
+  let touches = [];
+  let pressed = [];
 
-	let sampler;
-	// We should load the sampler (the thing that makes sounds) first after the user have interacted with website.
-	// Some browsers disable sound before that.
-	function loadSampler() {
-		sampler = createSampler();
-	}
+  let sampler;
+  // We should load the sampler (the thing that makes sounds) first after the user have interacted with website.
+  // Some browsers disable sound before that.
+  function loadSampler() {
+    sampler = createSampler();
+  }
 
-	function playKey(e) {
-		if (e.target.id === clicked || !leftMouseIsPressed(e)) return;
-		clicked = e.target.id;
-		playSound(clicked);
-	}
+  function playKey(e) {
+    if (e.target.id === clicked || !leftMouseIsPressed(e)) return;
+    clicked = e.target.id;
+    playSound(clicked);
+  }
 
-	// We want to trigger on left mouse button, regardless if other buttons are pressed.
-	function leftMouseIsPressed(e) {
-		let buttons = e.buttons;
-		if (buttons >= 16) {
-			buttons -= 16;
-		}
-		if (buttons >= 8) {
-			buttons -= 8;
-		}
-		if (buttons >= 4) {
-			buttons -= 4;
-		}
-		if (buttons >= 2) {
-			buttons -= 2;
-		}
-		return buttons === 1;
-	}
-	function stopKeyMove(e) {
-		if (!leftMouseIsPressed(e)) return;
-		clicked = null;
-		stopSound(e.target.id);
-	}
-	function stopKeyUp(e) {
-		stopSound(e.target.id);
-		clicked = null;
-	}
-	function handleTouch(event) {
-		const touch = event.detail.touch;
-		touches.push(touch);
-		touches = touches;
-		playSound(touch);
-	}
-	function handleMoved(event) {
-		touches = touches.filter(
-			(touch) => !touch.includes(event.detail.released)
-		);
-		touches = [...event.detail.pressed, ...touches];
-		for (const press of event.detail.pressed) {
-			playSound(press);
-		}
-		for (const release of event.detail.released) {
-			stopSound(release);
-		}
-	}
-	function handleRelease(event) {
-		const releasedNote = event.detail.released;
-		stopSound(releasedNote);
-		touches = touches.filter((el) => el !== releasedNote);
-	}
-	function idToNote(id) {
-		const newNum = id[id.length - 1] - 2;
-		return id.slice(0, -1) + newNum;
-	}
-	function playSound(id) {
-		if (!sampler) return;
-		const note = idToNote(id);
-		try {
-			sampler.triggerAttack(note);
-		} catch (error) {
-			console.error(error);
-			showSoundMessage = true;
-			setTimeout(() => {
-				showSoundMessage = false;
-			}, 2500);
-		}
-	}
-	function stopSound(id) {
-		if (!sampler) return;
-		const note = idToNote(id);
-		sampler.triggerRelease(note);
-	}
-	function keyDown({ detail: { note } }) {
-		playSound(note);
-		pressed = [...pressed, note];
-	}
-	function keyUp({ detail: { note } }) {
-		stopSound(note);
-		pressed = pressed.filter((el) => el !== note);
-	}
-	function addSharp(note) {
-		return note[0] + "#" + note[1];
-	}
+  // We want to trigger on left mouse button, regardless if other buttons are pressed.
+  function leftMouseIsPressed(e) {
+    let buttons = e.buttons;
+    if (buttons >= 16) {
+      buttons -= 16;
+    }
+    if (buttons >= 8) {
+      buttons -= 8;
+    }
+    if (buttons >= 4) {
+      buttons -= 4;
+    }
+    if (buttons >= 2) {
+      buttons -= 2;
+    }
+    return buttons === 1;
+  }
+  function stopKeyMove(e) {
+    if (!leftMouseIsPressed(e)) return;
+    clicked = null;
+    stopSound(e.target.id);
+  }
+  function stopKeyUp(e) {
+    stopSound(e.target.id);
+    clicked = null;
+  }
+  function handleTouch(event) {
+    const touch = event.detail.touch;
+    touches.push(touch);
+    touches = touches;
+    playSound(touch);
+  }
+  function handleMoved(event) {
+    touches = touches.filter((touch) => !touch.includes(event.detail.released));
+    touches = [...event.detail.pressed, ...touches];
+    for (const press of event.detail.pressed) {
+      playSound(press);
+    }
+    for (const release of event.detail.released) {
+      stopSound(release);
+    }
+  }
+  function handleRelease(event) {
+    const releasedNote = event.detail.released;
+    stopSound(releasedNote);
+    touches = touches.filter((el) => el !== releasedNote);
+  }
+  function idToNote(id) {
+    const newNum = id[id.length - 1] - 2;
+    return id.slice(0, -1) + newNum;
+  }
+  function playSound(id) {
+    if (!sampler) return;
+    const note = idToNote(id);
+    try {
+      sampler.triggerAttack(note);
+    } catch (error) {
+      console.error(error);
+      showSoundMessage = true;
+      setTimeout(() => {
+        showSoundMessage = false;
+      }, 2500);
+    }
+  }
+  function stopSound(id) {
+    if (!sampler) return;
+    const note = idToNote(id);
+    sampler.triggerRelease(note);
+  }
+  function keyDown({ detail: { note } }) {
+    playSound(note);
+    pressed = [...pressed, note];
+  }
+  function keyUp({ detail: { note } }) {
+    stopSound(note);
+    pressed = pressed.filter((el) => el !== note);
+  }
+  function addSharp(note) {
+    return note[0] + "#" + note[1];
+  }
 </script>
 
 <Window {keys} {translation} on:keydown={keyDown} on:keyup={keyUp} />
 <div id="container" class:fullscreen>
-	<svg
-		class:fullscreen
-		viewbox="-1 -1 {width} 302"
-		preserveAspectRatio="none"
-		use:touchable
-		on:touched={handleTouch}
-		on:moved={handleMoved}
-		on:released={handleRelease}
-	>
-		<g id="white-keys">
-			{#each keys as key, i}
-				<path
-					class:activeWhite={clicked === key.note ||
-						touches.includes(key.note) ||
-						pressed.includes(key.note)}
-					on:mousedown={playKey}
-					on:mousemove={playKey}
-					on:mouseleave={stopKeyMove}
-					on:mouseup={stopKeyUp}
-					id={key.note}
-					d={i === 0
-						? "M0 0 v290 a 10 10 0 0 0 10 10 h90 v-300 Z"
-						: i === keys.length - 1
-						? `M${i * 100} 0 v300 h90 a 10 10 0 0 0 10 -10 v-290 Z`
-						: `M${i * 100} 0 v300 h100 v-300 Z`}
-				/>
+  <svg
+    class:fullscreen
+    viewbox="-1 -1 {width} 302"
+    preserveAspectRatio="none"
+    use:touchable
+    on:touched={handleTouch}
+    on:moved={handleMoved}
+    on:released={handleRelease}
+  >
+    <g id="white-keys">
+      {#each keys as key, i}
+        <path
+          class:activeWhite={clicked === key.note ||
+            touches.includes(key.note) ||
+            pressed.includes(key.note)}
+          on:mousedown={playKey}
+          on:mousemove={playKey}
+          on:mouseleave={stopKeyMove}
+          on:mouseup={stopKeyUp}
+          id={key.note}
+          d={i === 0
+            ? "M0 0 v290 a 10 10 0 0 0 10 10 h90 v-300 Z"
+            : i === keys.length - 1
+              ? `M${i * 100} 0 v300 h90 a 10 10 0 0 0 10 -10 v-290 Z`
+              : `M${i * 100} 0 v300 h100 v-300 Z`}
+        />
 
-				{#if showKeybindings}
-					<!-- Used to display keybinding  -->
-					<text
-						x={i * 100 + 38}
-						font-family="monospace"
-						font-size="30"
-						y="275"
-						fill="black"
-					>
-						{translation.find((el) => el.note === key.note).key}
-					</text>
-				{/if}
+        {#if showKeybindings}
+          <!-- Used to display keybinding  -->
+          <text
+            x={i * 100 + 38}
+            font-family="monospace"
+            font-size="30"
+            y="275"
+            fill="black"
+          >
+            {translation.find((el) => el.note === key.note).key}
+          </text>
+        {/if}
 
-				{#if showNotes}
-					<!-- Used to display note name  -->
-					<text
-						x={i * 100 + 38}
-						font-family="monospace"
-						font-size="30"
-						y="235"
-						fill="black">{key.note.slice(0, 1)}</text
-					>
-				{/if}
-			{/each}
-		</g>
-		<g id="black-keys">
-			{#each keys as key, i}
-				{#if key.hasNeighbour}
-					<path
-						class:activeBlack={clicked === addSharp(key.note) ||
-							touches.includes(addSharp(key.note)) ||
-							pressed.includes(addSharp(key.note))}
-						on:mousedown={playKey}
-						on:mousemove={playKey}
-						on:mouseleave={stopKeyMove}
-						on:mouseup={stopKeyUp}
-						id={addSharp(key.note)}
-						d="M{100 * i +
-							65} 0 v170 a 10 10 0 0 0 10 10 h50 a 10 10 0 0 0 10 -10 V0 Z"
-					/>
-					{#if showKeybindings}
-						<!-- Used to display keybinding name  -->
-						<text
-							x={i * 100 + 90}
-							font-family="monospace"
-							font-size="30"
-							y="165"
-							fill="white"
-							stroke="white"
-						>
-							{translation.find(
-								(el) => el.note === addSharp(key.note)
-							).key}
-						</text>
-					{/if}
+        {#if showNotes}
+          <!-- Used to display note name  -->
+          <text
+            x={i * 100 + 38}
+            font-family="monospace"
+            font-size="30"
+            y="235"
+            fill="black">{key.note.slice(0, 1)}</text
+          >
+        {/if}
+      {/each}
+    </g>
+    <g id="black-keys">
+      {#each keys as key, i}
+        {#if key.hasNeighbour}
+          <path
+            class:activeBlack={clicked === addSharp(key.note) ||
+              touches.includes(addSharp(key.note)) ||
+              pressed.includes(addSharp(key.note))}
+            on:mousedown={playKey}
+            on:mousemove={playKey}
+            on:mouseleave={stopKeyMove}
+            on:mouseup={stopKeyUp}
+            id={addSharp(key.note)}
+            d="M{100 * i +
+              65} 0 v170 a 10 10 0 0 0 10 10 h50 a 10 10 0 0 0 10 -10 V0 Z"
+          />
+          {#if showKeybindings}
+            <!-- Used to display keybinding name  -->
+            <text
+              x={i * 100 + 90}
+              font-family="monospace"
+              font-size="30"
+              y="165"
+              fill="white"
+              stroke="white"
+            >
+              {translation.find((el) => el.note === addSharp(key.note)).key}
+            </text>
+          {/if}
 
-					{#if showNotes}
-						<!-- Used to display note name  -->
-						<text
-							x={i * 100 + 82}
-							font-family="monospace"
-							font-size="30"
-							y="125"
-							fill="white"
-							stroke="white"
-						>
-							{addSharp(key.note).slice(0, 2)}</text
-						>
-					{/if}
-				{/if}
-			{/each}
-		</g>
-	</svg>
-	<Overlay bind:showSoundMessage on:loadSampler={loadSampler} {fullscreen} />
+          {#if showNotes}
+            <!-- Used to display note name  -->
+            <text
+              x={i * 100 + 82}
+              font-family="monospace"
+              font-size="30"
+              y="125"
+              fill="white"
+              stroke="white"
+            >
+              {addSharp(key.note).slice(0, 2)}</text
+            >
+          {/if}
+        {/if}
+      {/each}
+    </g>
+  </svg>
+  <Overlay bind:showSoundMessage on:loadSampler={loadSampler} {fullscreen} />
 </div>
 
 <style>
-	#loading-sound-info {
-		padding-bottom: 5px;
-	}
-	#container {
-		position: relative;
-		margin: 0 auto 2em auto;
-		padding-top: 5px;
-	}
-	@media screen and (min-width: 1000px) {
-		#container {
-			max-width: 80vw;
-		}
-	}
+  #loading-sound-info {
+    padding-bottom: 5px;
+  }
+  #container {
+    position: relative;
+    margin: 0 auto 2em auto;
+    padding-top: 5px;
+  }
+  @media screen and (min-width: 1000px) {
+    #container:has(svg > #white-keys > path:nth-child(4)) {
+      max-width: 400px;
+      height: 220px;
+    }
+    #container:has(svg > #white-keys > path:nth-child(6)) {
+      max-width: 600px;
+      height: 220px;
+    }
 
-	#white-keys {
-		fill: white;
-		stroke: black;
-	}
-	#black-keys {
-		fill: black;
-		stroke: black;
-	}
-	.activeWhite {
-		fill: #ddd;
-	}
-	.activeBlack {
-		fill: #444;
-	}
-	svg.fullscreen {
-		height: 100%;
-		width: 100%;
-	}
-	#container.fullscreen {
-		position: absolute;
-		top: -10px;
-		background-color: green;
-		width: 100%;
-		height: calc(100% + 10px);
-		max-width: none;
-	}
+    #container:has(svg > #white-keys > path:nth-child(8)) {
+      max-width: 800px;
+      height: 240px;
+    }
 
-	/* @media only screen and (orientation: portrait) {
+    #container:has(svg > #white-keys > path:nth-child(10)) {
+      max-width: 1000px;
+      height: 260px;
+    }
+    #container:has(svg > #white-keys > path:nth-child(12)) {
+      max-width: 1200px;
+      height: 280px;
+    }
+
+    #container:has(svg > #white-keys > path:nth-child(14)) {
+      max-width: 1400px;
+      height: 320px;
+    }
+
+    #container:has(svg > #white-keys > path:nth-child(16)) {
+      max-width: 100%;
+    }
+  }
+
+  #white-keys {
+    fill: white;
+    stroke: black;
+  }
+  #black-keys {
+    fill: black;
+    stroke: black;
+  }
+  .activeWhite {
+    fill: #ddd;
+  }
+  .activeBlack {
+    fill: #444;
+  }
+  #container svg {
+    height: 100%;
+    width: 100%;
+    display: block;
+  }
+
+  #container.fullscreen {
+    position: absolute;
+    top: -10px;
+    background-color: green;
+    width: 100%;
+    height: calc(100% + 10px);
+    max-width: none !important;
+  }
+
+  /* @media only screen and (orientation: portrait) {
 		#container.fullscreen {
 			width: 100vh;
 			height: 100vw;
--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,136 +1,135 @@
 <script>
-	let notes = [
-		"G3",
-		"A3",
-		"B3",
-		"C4",
-		"D4",
-		"E4",
-		"F4",
-		"G4",
-		"A4",
-		"B4",
-		"C5",
-		"D5",
-		"E5",
-		"F5",
-		"G5",
-		"A5",
-		"B5",
-		"C6",
-		"D6",
-		"E6",
-		"F6",
-		"G6",
-		"A6",
-	];
-	let lowestNote = localStorage.getItem("lowestNote");
-	let highestNote = localStorage.getItem("highestNote");
-	$: {
-		if (!(notes.includes(lowestNote) && notes.includes(highestNote))) {
-			lowestNote = "C4";
-			highestNote = "C5";
-		}
-		localStorage.setItem("lowestNote", lowestNote);
-		localStorage.setItem("highestNote", highestNote);
-	}
-	export let showKeybindings = false;
-	export let showNotes = false;
-	export let displayedNotes = notes.slice(
-		notes.indexOf(lowestNote),
-		notes.indexOf(highestNote) + 1
-	);
+  let notes = [
+    "G3",
+    "A3",
+    "B3",
+    "C4",
+    "D4",
+    "E4",
+    "F4",
+    "G4",
+    "A4",
+    "B4",
+    "C5",
+    "D5",
+    "E5",
+    "F5",
+    "G5",
+    "A5",
+    "B5",
+    "C6",
+    "D6",
+    "E6",
+    "F6",
+    "G6",
+    "A6",
+  ];
+  let lowestNote = localStorage.getItem("lowestNote");
+  let highestNote = localStorage.getItem("highestNote");
+  $: {
+    if (!(notes.includes(lowestNote) && notes.includes(highestNote))) {
+      lowestNote = "C4";
+      highestNote = "C5";
+    }
+    localStorage.setItem("lowestNote", lowestNote);
+    localStorage.setItem("highestNote", highestNote);
+  }
+  export let showKeybindings = false;
+  export let showNotes = false;
+  export let displayedNotes = notes.slice(
+    notes.indexOf(lowestNote),
+    notes.indexOf(highestNote) + 1,
+  );
 
-	$: displayedNotes = notes.slice(
-		notes.indexOf(lowestNote),
-		notes.indexOf(highestNote) + 1
-	);
-	export let showSettings;
+  $: displayedNotes = notes.slice(
+    notes.indexOf(lowestNote),
+    notes.indexOf(highestNote) + 1,
+  );
+  export let showSettings;
 </script>
 
-<!-- <div class:hide={!showSettings} id="container"> -->
-<div class:hide={!showSettings} id="container">
-	<div class="setting">
-		<label for="lowestNote">Select lowest note:</label>
-		<select bind:value={lowestNote} name="lowestNote" id="lowestNote">
-			{#each notes.slice(0, notes.indexOf(highestNote)) as note, key}
-				<option selected={note === lowestNote} {key} value={note}
-					>{note}
-				</option>
-			{/each}
-		</select>
-	</div>
-	<div class="setting">
-		<label for="highestNote">Select highest note:</label>
-		<select bind:value={highestNote} name="highestNote" id="highestNote">
-			{#each notes.slice(notes.indexOf(lowestNote) + 1) as note, key}
-				<option selected={note === highestNote} {key} value={note}
-					>{note}
-				</option>
-			{/each}
-		</select>
-	</div>
-	<div class="setting">
-		<label for="showNotes">Show Note Values:</label>
-		<div>
-			<input
-				bind:checked={showNotes}
-				type="checkbox"
-				id="showNotes"
-				name="showNotes"
-			/>
-		</div>
-	</div>
-	<div class="setting">
-		<label for="showKeybindings">Show Keybindings:</label>
-		<div>
-			<input
-				bind:checked={showKeybindings}
-				type="checkbox"
-				id="showKeybindings"
-				name="showKeybindings"
-			/>
-		</div>
-	</div>
+<div class:hide={!showSettings} id="settings-container">
+  <div class="setting">
+    <label for="lowestNote">Select lowest note:</label>
+    <select bind:value={lowestNote} name="lowestNote" id="lowestNote">
+      {#each notes.slice(0, notes.indexOf(highestNote)) as note, key}
+        <option selected={note === lowestNote} {key} value={note}
+          >{note}
+        </option>
+      {/each}
+    </select>
+  </div>
+  <div class="setting">
+    <label for="highestNote">Select highest note:</label>
+    <select bind:value={highestNote} name="highestNote" id="highestNote">
+      {#each notes.slice(notes.indexOf(lowestNote) + 1) as note, key}
+        <option selected={note === highestNote} {key} value={note}
+          >{note}
+        </option>
+      {/each}
+    </select>
+  </div>
+  <div class="setting">
+    <label for="showNotes">Show Note Values:</label>
+    <div>
+      <input
+        bind:checked={showNotes}
+        type="checkbox"
+        id="showNotes"
+        name="showNotes"
+      />
+    </div>
+  </div>
+  <div class="setting">
+    <label for="showKeybindings">Show Keybindings:</label>
+    <div>
+      <input
+        bind:checked={showKeybindings}
+        type="checkbox"
+        id="showKeybindings"
+        name="showKeybindings"
+      />
+    </div>
+  </div>
 </div>
 
 <style>
-	#container {
-		border-radius: 25px;
-		margin: 10px 0 5px 0;
-		background-color: #ccc;
-		display: inline-block;
-		padding: 10px;
-		box-shadow: 0 0 8px 0 #000;
-	}
-	.setting {
-		display: grid;
-		grid-template-columns: 2fr 1fr;
-		align-items: center;
-		margin: 4px;
-		padding: 4px;
-	}
-	select,
-	input {
-		padding: 0;
-		margin: 0;
-	}
+  #settings-container {
+    border-radius: 25px;
+    margin: 10px 0 5px 0;
+    background-color: #ccc;
+    display: inline-block;
+    padding: 10px;
+    box-shadow: 0 0 8px 0 #000;
+  }
+  .setting {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    align-items: center;
+    margin: 4px;
+    padding: 4px;
+  }
+  select,
+  input {
+    padding: 0;
+    margin: 0;
+  }
 
-	label {
-		margin-right: 5px;
-	}
-	@media screen and (max-width: 600px) {
-		label {
-			margin-right: 0;
-		}
-		.setting {
-			display: block;
-		}
-		select {
-			min-width: 80px;
-		}
-	}
-	#container.hide {
-		display: none;
-	}
+  label {
+    margin-right: 5px;
+  }
+  @media screen and (max-width: 600px) {
+    label {
+      margin-right: 0;
+    }
+    .setting {
+      display: block;
+    }
+    select {
+      min-width: 80px;
+    }
+  }
+  #settings-container.hide {
+    display: none;
+  }
 </style>
